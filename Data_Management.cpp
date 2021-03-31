1c3890c5abe4f3c28ed5c0e541f85aa1fe0352b4
Michel Gonzalez <gonzalezmike96@gmail.com>
2021-03-31 08:57:08 -0400

    Added some more strucutral code to the different file, included the data to be sorted

--- Data_Management.cpp
+++ Data_Management.cpp
@@ -1,25 +1,28 @@
 //FINAL_PROJECT_DATA_MANAGEMENT
 # include<fstream>
+#include<iostream>
 
-const std::string FILE_ONE = "file_one";
+//const std::string FILE_ONE = "file_one";
 
 class Data_Management{
-	void read_data(std::ifstream file){
-		if(
+	std::string read_data(std::ifstream file){
+		std::string line = "";
+		if(std::getline(file, line) == false){
+
+		}else{
+			line
+		}
 	}
 	//user_id (integer), item_id(integer), rating(integer), time_stamp(integer), title(string).
-	void static deserialize_data(bool user_id, bool item_id, bool rating, bool time_stamp, bool title){
-		try{
-		//could do with for loop but it'd be fun to use a recursive function...
+	std::string deserialize_data(std::string file_name){
 		std::ifstream file;
-		//do this same process for each file for appropriate data
-		file.open(FILE_ONE);
-		//ifstream myfile (FILE_ONE, ios::binary);
-		read_data(file);
-		
+		try{
+		file.open(file_name);
 		}catch(std::string e){
 			std::cout<<e<<std::endl;
+			return;
 		}
+		read_data(file);
 	}
 	
 	
