dfe5fc71092ea21e769d5578ac9c9658b5e8788e
Michel Gonzalez <gonzalezmike96@gmail.com>
2021-04-03 07:02:42 -0400

    Changes pre-commited 4-13-2021

--- Review.cpp
+++ Review.cpp
@@ -2,18 +2,28 @@ class Review{
 private:
 	//user_id (integer), item_id(integer), rating(integer), time_stamp(integer), title(string).
 	int user_id = 0;
-	int item_id = 0;
-	float rating = 0.0;//should it really be int?
+	int rating = 0;
 	int time_stamp = 0;
 	int movie_id = 0;
 	std::string movie_title ="";
 
 public:
-	int* get_userId(){ //pass by pointer for speed boost!
+	int get_userId(){ //pass by pointer for speed boost!
 		return this-> user_id;
 	}
-	get_itemId(){
-		
-	}
 	
+	int  get_movieId(){
+		return this -> movie_id;
+	}
+
+	int get_rating(){
+		return this -> rating;
+	}
+
+	int get_timeStamp(){
+
+	}
+	std::string get_movieTitle(){
+		return this -> movie_id;
+	}
 };
--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 //FINAL_PROJECT_MAIN
 
-int main(){
+int[] createMyData(){
+    int arr[];
+    switch(){
+        
+    }
+    return arr;
+}
 
+int main(){
+    int mydata[] = createMyData();
+    merge_sort(mydata);
+    quick_sort(my data);
 }
