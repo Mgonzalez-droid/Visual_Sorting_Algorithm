00638bdc06770d3d602c72e8920ba0803e429592
Michel Gonzalez <gonzalezmike96@gmail.com>
2021-04-05 10:38:47 -0400

    Finished quick sort Ascending, should be minor change to get descending to work, will have to test in website given in notes. Additionally, created a string comparison function to compare movie titles see description for more details

--- Quick_Sort_Descending.cpp
+++ Quick_Sort_Descending.cpp
@@ -2,14 +2,10 @@
 //Sort values in Descending order
 struct Quick_Sort_Descending{
     Quick_Sort_Descending(std::vector<int>& data, int low, int high){
-        //this -> low_i = low;
-        //this -> high_i = high;
         sort_i(data, low, high);
     }
 
     Quick_Sort_Descending(std::vector<std::string>& data, int low, int high){
-        //this ->low_s = low;
-        //this ->high_s = high;
         sort_s(data, low, high);
     }
 
--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include<iostream>
 #include"Data_Management.cpp"
 #include"Review.cpp"
-#include"Merge_Sort_min.cpp"
-#include"Merge_Sort_max.cpp"
-#include"Quick_Sort_min.cpp"
-#include"Quick_Sort_max.cpp"
+#include"Merge_Sort_Descending.cpp"//min
+#include"Merge_Sort_Ascending.cpp"//max
+#include"Quick_Sort_Descending.cpp"//min
+#include"Quick_Sort_Ascending.cpp"//max
 #include<map>
 #include<vector>
 //create a map of key string, val vector<int or string> therefore template needed
@@ -17,13 +17,14 @@ const int TIME_STAMP = 4;
 const int MOVIE_TITLE = 5;
 const int QUICK_SORT = 6;
 const int MERGE_SORT = 7;
-const int MIN = 8;
-const int MAX = 9;
+const int DESCENDING = 8;
+const int ASCENDING = 9;
 
 template<class t>
- void printMydata(t* reviews){
-     std::cout<<*reviews<<std::endl;
- }
+void print_results(std::vector<t> my_data){
+    for(int i = 0; i < my_data.size(); i++)
+        std::cout<<my_data[i]<<std::endl;
+}
 
 //user_id (integer), item_id(integer), rating(integer), time_stamp(integer), movie_title(string).
 void sort_data(int choice, int sort_method, int sort_property){
@@ -34,16 +35,22 @@ void sort_data(int choice, int sort_method, int sort_property){
             if(sort_method == MERGE_SORT){
                 int start = 0;
                 int end = my_data.size() - 1;
-                if(sort_property == MAX){
-                    //merge sort max
-                }else if(sort_property == MIN){
-                    //merge sort min
+                if(sort_property == ASCENDING){
+                    Merge_Sort_Ascending merge(my_data, start, end);
+                    print_results(my_data);
+                }else if(sort_property == DESCENDING){
+                    Merge_Sort_Descending merge(my_data, start, end);
+                    print_results(my_data);
                 }
             } else if(sort_method == QUICK_SORT){
-                if(sort_property == MAX){
-                    //quick sort max
-                }else if(sort_property == MIN){
-                    //quick sort min
+                int low = 0;
+                int high = my_data.size() / sizeof(my_data[0]);
+                if(sort_property == ASCENDING){
+                    Quick_Sort_Ascending quick(my_data,low, high);
+                    print_results(my_data);
+                }else if(sort_property == DESCENDING){
+                    Quick_Sort_Descending quick(my_data,low, high);
+                    print_results(my_data);
                 }
             }
         };
@@ -54,16 +61,22 @@ void sort_data(int choice, int sort_method, int sort_property){
             if(sort_method == MERGE_SORT){
                 int start = 0;
                 int end = my_data.size() - 1;
-                if(sort_property == MAX){
-                    //merge sort max
-                }else if(sort_property == MIN){
-                    //merge sort min
+                if(sort_property == ASCENDING){
+                    Merge_Sort_Ascending merge(my_data, start, end);
+                    print_results(my_data);
+                }else if(sort_property == DESCENDING){
+                    Merge_Sort_Descending merge(my_data, start, end);
+                    print_results(my_data);
                 }
             } else if(sort_method == QUICK_SORT){
-                if(sort_property == MAX){
-                    //quick sort max
-                }else if(sort_property == MIN){
-                    //quick sort min
+                int low = 0;
+                int high = my_data.size() / sizeof(my_data[0]);
+                if(sort_property == ASCENDING){
+                    Quick_Sort_Ascending quick(my_data,low, high);
+                    print_results(my_data);
+                }else if(sort_property == DESCENDING){
+                    Quick_Sort_Descending quick(my_data,low, high);
+                    print_results(my_data);
                 }
             }
         };
@@ -74,16 +87,22 @@ void sort_data(int choice, int sort_method, int sort_property){
             if(sort_method == MERGE_SORT){
                 int start = 0;
                 int end = my_data.size() - 1;
-                if(sort_property == MAX){
-                    //merge sort max
-                }else if(sort_property == MIN){
-                    //merge sort min
+                if(sort_property == ASCENDING){
+                    Merge_Sort_Ascending merge(my_data, start, end);
+                    print_results(my_data);
+                }else if(sort_property == DESCENDING){
+                    Merge_Sort_Descending merge(my_data, start, end);
+                    print_results(my_data);
                 }
             } else if(sort_method == QUICK_SORT){
-                if(sort_property == MAX){
-                    //quick sort max
-                }else if(sort_property == MIN){
-                    //quick sort min
+                int low = 0;
+                int high = my_data.size() / sizeof(my_data[0]);
+                if(sort_property == ASCENDING){
+                    Quick_Sort_Ascending quick(my_data,low, high);
+                    print_results(my_data);
+                }else if(sort_property == DESCENDING){
+                    Quick_Sort_Descending quick(my_data,low, high);
+                    print_results(my_data);
                 }
             }
         };
@@ -91,19 +110,25 @@ void sort_data(int choice, int sort_method, int sort_property){
 
         case TIME_STAMP :{
             std::vector<int> my_data = data.getTimeStampVec();
-            if(sort_method == MERGE_SORT){
+             if(sort_method == MERGE_SORT){
                 int start = 0;
                 int end = my_data.size() - 1;
-                if(sort_property == MAX){
-                    //merge sort max
-                }else if(sort_property == MIN){
-                    //merge sort min
+                if(sort_property == ASCENDING){
+                    Merge_Sort_Ascending merge(my_data, start, end);
+                    print_results(my_data);
+                }else if(sort_property == DESCENDING){
+                    Merge_Sort_Descending merge(my_data, start, end);
+                    print_results(my_data);
                 }
             } else if(sort_method == QUICK_SORT){
-                if(sort_property == MAX){
-                    //quick sort max
-                }else if(sort_property == MIN){
-                    //quick sort min
+                int low = 0;
+                int high = my_data.size() / sizeof(my_data[0]);
+                if(sort_property == ASCENDING){
+                    Quick_Sort_Ascending quick(my_data,low, high);
+                    print_results(my_data);
+                }else if(sort_property == DESCENDING){
+                    Quick_Sort_Descending quick(my_data,low, high);
+                    print_results(my_data);
                 }
             }
         };
@@ -114,16 +139,22 @@ void sort_data(int choice, int sort_method, int sort_property){
             if(sort_method == MERGE_SORT){
                 int start = 0;
                 int end = my_data.size() - 1;
-                if(sort_property == MAX){
-                    //merge sort max
-                }else if(sort_property == MIN){
-                    //merge sort min
+                if(sort_property == ASCENDING){
+                    Merge_Sort_Ascending merge(my_data, start, end);
+                    print_results(my_data);
+                }else if(sort_property == DESCENDING){
+                    Merge_Sort_Descending merge(my_data, start, end);
+                    print_results(my_data);
                 }
             } else if(sort_method == QUICK_SORT){
-                if(sort_property == MAX){
-                    //quick sort max
-                }else if(sort_property == MIN){
-                    //quick sort min
+                int low = 0;
+                int high = my_data.size() / sizeof(my_data[0]);
+                if(sort_property == ASCENDING){
+                    Quick_Sort_Ascending quick(my_data,low, high);
+                    print_results(my_data);
+                }else if(sort_property == DESCENDING){
+                    Quick_Sort_Descending quick(my_data,low, high);
+                    print_results(my_data);
                 }
             }
         };
@@ -142,21 +173,21 @@ int main(){
     */
    int choice = 0;//whatever is taken in by the UI
    int sort_method = QUICK_SORT;
-   int sort_property = MIN;
+   int sort_property = DESCENDING;
 
    //sort_data(choice, true);
 
     //TODO: define variables for UI
     //quick sort min
-    sort_data(choice, QUICK_SORT, MIN);
+    sort_data(choice, QUICK_SORT, DESCENDING);
 
     //quick sort max
-    sort_data(choice, QUICK_SORT, MAX);
+    sort_data(choice, QUICK_SORT, ASCENDING);
 
     //merge sort min
-    sort_data(choice, MERGE_SORT, MIN);
+    sort_data(choice, MERGE_SORT, DESCENDING);
 
     //merge sort max
-    sort_data(choice, MERGE_SORT, MAX);
+    sort_data(choice, MERGE_SORT, ASCENDING);
     
 }
