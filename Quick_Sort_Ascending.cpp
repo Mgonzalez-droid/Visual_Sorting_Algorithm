63d225815a38955ca15b331be77f8d2407c065dc
Michel Gonzalez <gonzalezmike96@gmail.com>
2021-04-05 11:29:01 -0400

    Originally implemented a quick sort algorithm in merge sort, fixed this issue. Successfully implemented Quick Sort for both Ascending and Descending cases. Will have to test this with brought in data. Currently Works in notes build

--- Quick_Sort_Ascending.cpp
+++ Quick_Sort_Ascending.cpp
@@ -9,15 +9,7 @@ struct Quick_Sort_Ascending{
         sort_s(data, low, high);
     }
 
-    void sort_s(std::vector<std::string>& data, int low, int high){
-
-    }
-
-    void sort_i(std::vector<int>& data, int low, int high){
-        
-    }
-
-        //FROM NOTES: CITE?
+    //FROM NOTES: CITE?
 
     //____________INTEGERS_____________
     void sort_i(std::vector<int>& data, int low, int high) {
