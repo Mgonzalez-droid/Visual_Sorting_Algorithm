e12749831298e640da6e690e911dee2ab7259439
Michel Gonzalez <gonzalezmike96@gmail.com>
2021-04-13 06:01:25 -0400

    made changes to code layout, will need to move helper classes into seperate classes to reduce the ammount of clutter in main.cpp. Need to fix issue with bucket sort and merge sort being called twice for 1 A. need to create bucket sort algorithm.

--- main.cpp
+++ main.cpp
@@ -25,13 +25,63 @@ const int BUCKET_SORT = 8;
 const int DESCENDING = 9;
 const int ASCENDING = 10;
 
+//For Debugging purposes
 template<class t>
 void print_results(std::vector<t>);
 
+//Sorting helper methods
+//TODO:condense with templates, move into seperate class: Runtime_Sort_Configuration.cpp (all static)
+void sort_data(int,int);
 void mergeSort(std::vector<int>, int);
-//continue this for all methods, place all under main
+void mergeSort(std::vector<std::string>, int);
+void quickSort(std::vector<int>, int);
+void quickSort(std::vector<std::string>, int);
+void bucketSort(std::vector<int>, int);
+void bucketSort(std::vector<std::string>, int);
 
-//FILE CREATION AND DELETION
+//Screens
+//TODO: move into seperate class: Command_Line_UI.cpp (all static)
+std::pair<int,char> mainScreen();
+void clearScreen();
+void sortingSplashScreen();
+char closingScreen();
+
+int main(){
+
+    bool continue_sorting = true;
+    char quit = 'c';
+
+    std::pair<int,char> sortingPreferences;
+
+    while(continue_sorting){
+        sortingPreferences = mainScreen();
+        //clearScreen(); need to troubleshoot
+        if( sortingPreferences.first == -1){
+            continue_sorting = false;
+        } else{
+
+            sortingSplashScreen();
+
+            sort_data(sortingPreferences.first, ASCENDING);
+            sort_data(sortingPreferences.first, DESCENDING);
+
+        }
+        quit = closingScreen();
+        if(quit == 'n' || quit == 'N'){
+            continue_sorting = false;
+        }
+    }
+}
+
+//For debugging
+template<class t>
+void print_results(std::vector<t> my_data){
+    for(int i = 0; i < my_data.size(); i++)
+        std::cout<<my_data[i]<<std::endl;
+}
+
+//For file creation and deletion
+
 template<class t>
 void createReport(std::vector<t> results,int sortingMethod, int sort_property){
     switch(sortingMethod){
@@ -139,6 +189,75 @@ void createReport(std::vector<t> results,int sortingMethod, int sort_property){
     }
 }
 
+//Sorting and sorting helper methods
+
+void sort_data(int choice, int sort_property){
+    Data_Management data;
+    switch(choice){
+        case USER_ID :{
+            std::vector<int> my_data = data.getUserIdVec();
+            mergeSort(my_data,sort_property);
+            quickSort(my_data,sort_property);
+            bucketSort(my_data,sort_property);
+        };
+            break;
+
+        case ITEM_ID :{
+            std::vector<int> my_data = data.getItemIdVec();
+            mergeSort(my_data,sort_property);
+            quickSort(my_data,sort_property);
+            bucketSort(my_data,sort_property);
+        };
+            break;
+
+        case RATING :{
+            std::vector<int> my_data = data.getRatingVec();
+            mergeSort(my_data,sort_property);
+            quickSort(my_data,sort_property);
+            bucketSort(my_data,sort_property);
+        };
+            break;
+
+        case TIME_STAMP :{
+            std::vector<int> my_data = data.getTimeStampVec();
+            mergeSort(my_data,sort_property);
+            quickSort(my_data,sort_property);
+            bucketSort(my_data,sort_property);
+        };
+            break;
+
+        case MOVIE_TITLE :{
+            std::vector<std::string> my_data = data.getMovieTitleVec();
+            mergeSort(my_data,sort_property);
+            quickSort(my_data,sort_property);
+            bucketSort(my_data,sort_property);
+        };
+            break;
+    }
+}
+
+void mergeSort(std::vector<int> my_data, int sort_property){
+    //start time
+    time_t t_start = time(nullptr);
+
+    int start = 0;
+    int end = my_data.size() - 1;
+    if(sort_property == ASCENDING){
+        Merge_Sort_Ascending merge(my_data, start, end);
+        //print_results(my_data);
+    }else if(sort_property == DESCENDING){
+        Merge_Sort_Descending merge(my_data, start, end);
+        //print_results(my_data);
+    }
+
+    //end time
+    time_t t_end = time(nullptr);
+    //output time difference
+    std::cout<<std::endl<<"Merge Sort took "<< t_end - t_start <<" second(s)."<<std::endl;
+    //print results to text file
+    createReport(my_data,MERGE_SORT,sort_property);
+}
+
 void mergeSort(std::vector<std::string> my_data, int sort_property){
     //start time
     time_t t_start = time(nullptr);
@@ -156,7 +275,7 @@ void mergeSort(std::vector<std::string> my_data, int sort_property){
     //end time
     time_t t_end = time(nullptr);
     //output time difference
-    std::cout<<"Merge Sort took "<< t_end - t_start << " second(s)."<<std::endl;
+    std::cout<<std::endl<<"Merge Sort took "<< t_end - t_start << " second(s)."<<std::endl;
     //print to text file
     createReport(my_data,MERGE_SORT,sort_property);
 }
@@ -178,7 +297,7 @@ void quickSort(std::vector<int> my_data, int sort_property){
     //end time
     time_t t_end = time(nullptr);
     //output time difference
-    std::cout<< "Quick Sort took " << t_end - t_start << " second(s)." <<std::endl;
+    std::cout<<std::endl<< "Quick Sort took " << t_end - t_start << " second(s)." <<std::endl;
     //print results to text file
     createReport(my_data,QUICK_SORT,sort_property);
 }
@@ -200,7 +319,7 @@ void quickSort(std::vector<std::string> my_data, int sort_property){
     //end time
     time_t t_end = time(nullptr);
     //output time difference
-    std::cout<<"Quick Sort took "<<t_end - t_start<<" second(s)."<<std::endl;
+    std::cout<<std::endl<<"Quick Sort took "<<t_end - t_start<<" second(s)."<<std::endl;
     //print results to text file
     createReport(my_data,QUICK_SORT,sort_property);
 }
@@ -212,7 +331,7 @@ void bucketSort(std::vector<int> my_data, int sort_property){
     //end time
     time_t t_end = time(nullptr);
     //output time difference
-    std::cout<<"Bucket Sort took "<< t_end - t_start <<" second(s)."<<std::endl;
+    std::cout<<std::endl<<"Bucket Sort took "<< t_end - t_start <<" second(s)."<<std::endl;
     //print results to text file
     createReport(my_data,BUCKET_SORT,sort_property);
 }
@@ -224,57 +343,12 @@ void bucketSort(std::vector<std::string> my_data, int sort_property){
     //end time
     time_t t_end = time(nullptr);
     //output time difference
-    std::cout<<"Bucket Sort took "<< t_end - t_start <<" second(s)."<<std::endl;
+    std::cout<<std::endl<<"Bucket Sort took "<< t_end - t_start <<" second(s)."<<std::endl;
     //print results to text file
     createReport(my_data,BUCKET_SORT,sort_property);
 }
 
-//user_id (integer), item_id(integer), rating(integer), time_stamp(integer), movie_title(string).
-void sort_data(int choice, int sort_property){
-    Data_Management data;
-    switch(choice){
-        case USER_ID :{
-            std::vector<int> my_data = data.getUserIdVec();
-            mergeSort(my_data,sort_property);
-            quickSort(my_data,sort_property);
-            bucketSort(my_data,sort_property);
-        };
-        break;
-
-        case ITEM_ID :{
-            std::vector<int> my_data = data.getItemIdVec();
-            mergeSort(my_data,sort_property);
-            quickSort(my_data,sort_property);
-            bucketSort(my_data,sort_property);
-        };
-        break;
-
-        case RATING :{
-            std::vector<int> my_data = data.getRatingVec();
-            mergeSort(my_data,sort_property);
-            quickSort(my_data,sort_property);
-            bucketSort(my_data,sort_property);
-        };
-        break;
-
-        case TIME_STAMP :{
-            std::vector<int> my_data = data.getTimeStampVec();
-            mergeSort(my_data,sort_property);
-            quickSort(my_data,sort_property);
-            bucketSort(my_data,sort_property);
-        };
-        break;
-
-        case MOVIE_TITLE :{
-            std::vector<std::string> my_data = data.getMovieTitleVec();
-            mergeSort(my_data,sort_property);
-            quickSort(my_data,sort_property);
-            bucketSort(my_data,sort_property);
-        };
-        break;
-    }
-}
-
+//UI screen refresh
 
 std::pair<int,char> mainScreen(){
     std::pair<int,char> choices;
@@ -337,7 +411,7 @@ void sortingSplashScreen(){
     std::cout<<"Please wait currently sorting";
 }
 
-void closingScreen(){
+char closingScreen(){
     char choice = 'c';
     bool invalid = false;
     std::cout<<"Would you like to sort again? (y or Y for Yes, n or N for No): ";
@@ -355,58 +429,5 @@ void closingScreen(){
             }
         }
     }
-}
-
-
-int main(){
-
-    bool continue_sorting = true;
-
-    std::pair<int,char> sortingPreferences;
-
-    while(continue_sorting){
-        sortingPreferences = mainScreen();
-        //clearScreen(); need to troubleshoot
-        if( sortingPreferences.first == -1){
-            continue_sorting = false;
-        } else{
-
-            sortingSplashScreen();
-
-            sort_data(sortingPreferences.first, ASCENDING);
-            sort_data(sortingPreferences.first, DESCENDING);
-
-        }
-        closingScreen();
-    }
-}
-
-//Methods that make the magic happen go here... clean it up nicely!
-
-template<class t>
-void print_results(std::vector<t> my_data){
-    for(int i = 0; i < my_data.size(); i++)
-        std::cout<<my_data[i]<<std::endl;
-}
-
-void mergeSort(std::vector<int> my_data, int sort_property){
-    //start time
-    time_t t_start = time(nullptr);
-
-    int start = 0;
-    int end = my_data.size() - 1;
-    if(sort_property == ASCENDING){
-        Merge_Sort_Ascending merge(my_data, start, end);
-        //print_results(my_data);
-    }else if(sort_property == DESCENDING){
-        Merge_Sort_Descending merge(my_data, start, end);
-        //print_results(my_data);
-    }
-
-    //end time
-    time_t t_end = time(nullptr);
-    //output time difference
-    std::cout<<"Bucket Sort took "<< t_end - t_start <<" second(s)."<<std::endl;
-    //print results to text file
-    createReport(my_data,MERGE_SORT,sort_property);
+    return choice;
 }
